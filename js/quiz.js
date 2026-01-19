bff1dd1f378f8922960006d013ebfa1d355ab11c
emnabarraj04-ship-it <emnabarraj04@gmail.com>
2026-01-19 02:10:39 +0100

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,39 @@
+function checkQuiz() {
+
+    var reponses = {
+        q1: 'b',
+        q2: 'a',
+        q3: 'c',
+        q4: 'c',
+        q5: 'a',
+        q6: 'b',
+        q7: 'c',
+        q8: 'a',
+        q9: 'b',
+        q10: 'c'
+    };
+
+    var score = 0;
+    var total = 10;
+    var resultat = "";
+
+    for (var i = 1; i <= total; i++) {
+        var nomQuestion = "q" + i;
+        var choix = document.querySelector("input[name='" + nomQuestion + "']:checked");
+
+        if (choix) {
+            if (choix.value === reponses[nomQuestion]) {
+                score++;
+                resultat += "<li>Question " + i + " : Correct</li>";
+            } else {
+                resultat += "<li>Question " + i + " : Faux</li>";
+            }
+        } else {
+            resultat += "<li>Question " + i + " : Non répondue</li>";
+        }
+    }
+
+    document.getElementById("resultat").innerHTML =
+        "<h2>Votre score : " + score + " / " + total + "</h2><ul>" + resultat + "</ul>";
+}
 
